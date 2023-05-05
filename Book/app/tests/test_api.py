f36f4d1652e4c7450b1ab763314ba2ec16bbedb9
Marat <clever1mistory@gmail.com>
2023-05-05 22:59:53 +0300

    Filter, Search, Ordering добавлены в проект, unit tests

--- Book/app/tests/test_api.py
+++ Book/app/tests/test_api.py
@@ -7,12 +7,42 @@ from app.serializers import BookSerializer
 
 
 class BookApiTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.book1 = Book.objects.create(name='test book 1', price=25,
+                                         author_name='Author 1')
+        self.book2 = Book.objects.create(name='test book 2', price=26,
+                                         author_name='Author 5')
+        self.book3 = Book.objects.create(name='test book Author 1', price=26,
+                                         author_name='Author 3')
+
     def test_get(self):
-        book1 = Book.objects.create(name='test book 1', price= 25)
-        book2 = Book.objects.create(name= 'test book 2', price= 26)
         url = reverse('book-list')
         response = self.client.get(url)
-        serializer_data = BookSerializer([book1, book2], many=True).data
+        serializer_data = BookSerializer([self.book1, self.book2,
+                                          self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
+    def test_get_filter(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializer_data = BookSerializer([self.book1, self.book2,
+                                          self.book3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_search(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BookSerializer([self.book1,
+                                          self.book3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_order(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'ordering': 'name'})
+        serializer_data = BookSerializer([self.book1, self.book2,
+                                          self.book3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
--- Book/app/views.py
+++ Book/app/views.py
@@ -1,11 +1,17 @@
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from app.models import Book
 from app.serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['price']
+    search_fields = ['name', 'author_name']
+    ordering_fields = ['price', 'author_name']
 
 
